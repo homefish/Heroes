57083af943a4d5eda6f0480c1ce342ae40dd7ca9
homefish <powysto@gmail.com>
2018-09-30 19:21:17 +0200

    Added new hero and changed name of Spiderman.

--- Heroes/src/example/HeroExample.java
+++ Heroes/src/example/HeroExample.java
@@ -9,11 +9,13 @@ public class HeroExample {
 	public static void main(String[] args) {
 		final List<Hero> heroes = new ArrayList<>();
 
-		final Hero spiderman = new Hero("Peter Parker", 15, "Spidey sense");
+		final Hero spiderman = new Hero("Spiderman", 15, "Spidey sense");
 		final Hero thor = new Hero("Thor", 3000, "Lightning");
+		final Hero susanStorm = new Hero("Susan Storm", 23, "Invisibility");
 
 		heroes.add(spiderman);
 		heroes.add(thor);
+		heroes.add(susanStorm);
 
 		for (final Hero hero : heroes) {
 			System.out.println(hero);
